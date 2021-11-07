0cd5d10305696f1e7d4d6c37714f1292e9f4a508
oMatheusmol <80729568+oMatheusmol@users.noreply.github.com>
2021-11-07 20:42:58 -0300

    token refactoring

--- src/app/middlewares/auth.middleware.ts
+++ src/app/middlewares/auth.middleware.ts
@@ -1,28 +1,17 @@
 import { Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { get } from 'config';
+import IDecode from './interface/IDecode.interface';
 
 export const AuthMiddleware = (req: any, res: Response) => {
   const token = req.headers['x-api-key'];
   const secret: string = get('AUTH.SECRET');
-  //exist token
   if (token) {
-    //pre validation
-    // Verify token
-    const isValid = verify(token, secret, (err, decoded) => {
-      //invalid token (expired or other)
-      if (err) {
-        req.tokenError = `Token ${err.message}`;
-        throw new Error('Token is required. Access Denied');
-      } else {
-        // Save session in request
-        req.security = decoded.data;
-        return true;
-      }
+    verify(token, secret, (err:any, decoded:IDecode) => {
+      if (err) throw new Error('Token is required. Access Denied');
+      return req.security = decoded;
     });
-    return isValid;
   } else {
-    //token error
     throw new Error('Token is required. Access Denied');
   }
 };
---
+++ src/app/middlewares/interface/IDecode.interface.ts
@@ -0,0 +1,6 @@
+export default interface IDecode {
+    username: string,
+    exp: number,
+    iat: number,
+  }
+  
